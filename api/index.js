53b3b93b4eafafe4568b4fb54f824a6963abcb8f
AlvesGus <gustavo.tbt@hotmail.com.br>
2025-04-06 21:05:42 -0300

    fix: resolve cors origin from frontend

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://brevly-liart.vercel.app',
     allowedHeaders: ['Content-Type', 'Authorization'],
     AccessControlAllowOrigin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'DELETE']
